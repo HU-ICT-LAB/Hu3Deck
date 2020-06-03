97709543a7810afc57953fa7ba3f19b2a296c39d
Unknown <yasin1997@hotmail.com>
2020-06-03 15:03:52 +0200

    Refactored assign prop to scene

--- frontend/angular/src/app/assign-object/assign-object.component.ts
+++ frontend/angular/src/app/assign-object/assign-object.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder} from '@angular/forms';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-assign-object',
@@ -7,59 +8,66 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./assign-object.component.css']
 })
 export class AssignObjectComponent implements OnInit {
-  form1;
-  form2;
-  form3;
-  form4;
-  form5;
+    scenes = [];
+    form1;
+    selectScene: string = '';
 
-  constructor(private formBuilder: FormBuilder) {
+    prohibited = [
+        'Apex',
+        'Android'
+    ];
+
+    allowed= [
+        'Fortnite',
+        '90s',
+        'Bouwen',
+        'Noskin',
+        'Slurp juice',
+        'Boom bow',
+        'Quadruple ramp'
+    ];
+
+    constructor(private formBuilder: FormBuilder) {
+
+    this.scenes = [
+        "test",
+        "test2",
+    ]
 
     this.form1 = this.formBuilder.group({
-      selectScene: '',
-      duration: 0,
-      loop: '',
-      direction: ''
+      selectScene: ''
     });
 
-    this.form2 = this.formBuilder.group({
-      xPosition: 0,
-      yPosition: 0,
-      zPosition: 0
-    });
-
-    this.form3 = this.formBuilder.group({
-      xPositionTo: 0,
-      yPositionTo: 0,
-      zPositionTo: 0
-    });
-
-    this.form4 = this.formBuilder.group({
-      xScale: 0,
-      yScale: 0,
-      zScale: 0
-    });
-
-    this.form5 = this.formBuilder.group({
-      xRotation: 0,
-      yRotation: 0,
-      zRotation: 0
-    });
   }
 
   ngOnInit(): void {
   }
 
   save() {
-    console.log(this.form3.value);
+    console.log(this.form1.value);
   }
 
-  onSubmit(formData1, formData2, formData3, formData4, formData5){
+  onSubmit(formData1){
     console.log(formData1);
-    console.log(formData2);
-    console.log(formData3);
-    console.log(formData4);
-    console.log(formData5);
   }
+
+  onChangeScene(sceneValue) {
+      this.selectScene = sceneValue;
+      console.log(sceneValue);
+  }
+
+
+
+    drop(event: CdkDragDrop<string[]>) {
+        console.log(event.container['data'])
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
+        }
+    }
 
 }
