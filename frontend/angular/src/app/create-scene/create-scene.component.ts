069d0b6dff5df1934bf01c1ccec043f961769e71
lolzzzyo <lolzzzyo@hotmail.com>
2020-06-16 11:16:44 +0200

    fixed validation for create scene
    added validation to create scene, the scene name cannot be empty validation

--- frontend/angular/src/app/create-scene/create-scene.component.ts
+++ frontend/angular/src/app/create-scene/create-scene.component.ts
@@ -25,21 +25,24 @@ export class CreateSceneComponent implements OnInit {
 
 
   onSubmit(data) {
+    if(data.title != ""){
+        const options = {
+          headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+        };
 
-    const options = {
-      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-    };
 
+        const body = new HttpParams().set('title', data.title);
 
-    const body = new HttpParams().set('title', data.title);
-
-    this.http.post('http://localhost:3000/scenes/create', body.toString(), options).subscribe(dataa => {
-      console.log(dataa);
-    }, response => {
-      console.log(response);
-    });
-    console.log(data);
-  }
+        this.http.post('http://localhost:3000/scenes/create', body.toString(), options).subscribe(dataa => {
+          console.log(dataa);
+        }, response => {
+          console.log(response);
+        });
+        console.log(data);
+      }else{
+        alert("Scene name cannot be empty");
+      }
+    }
 
 
 }
